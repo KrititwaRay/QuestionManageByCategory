328c3d350b04d50253f605d2241466d2e1a02024
krititwa Ray <krititwaray97@gmail.com>
2024-11-08 22:10:45 +0530

    database connected

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
+import { ConnectDatabase } from './config/db_config.js';
+const connectDatabase = new ConnectDatabase();
+connectDatabase.connectMongoDb();
 
+app.use(express.json({ limit: "150mb" }))
+app.use((req, res, next) => {
+    const contentLength = req.headers['content-length'];
+    if (contentLength) {
+      console.log(`Incoming request size: ${contentLength} bytes`);
+    }
+    next();
+  });
 
 const PORT  = process.env.PORT || 3000;
 app.listen(PORT, () => {
